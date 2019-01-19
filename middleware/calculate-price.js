521965382ac74ec398fd0e9f3dfe8e1a5119d1fd
Aaron Shivers <aaron@moonmail.org>
2019-01-18 19:32:10 -0600

    changed margins

--- middleware/calculate-price.js
+++ middleware/calculate-price.js
@@ -15,35 +15,25 @@ const calculatePrice = (user, parameters) => {
   } = parameters
 
   // Operating Expenses
-  // const totalOperatingExpenses = 87084.17
+  // totalOperatingExpenses
 
   // Labor Expenses
-  // const payrollTaxExpenses = 9834.65
-  // const wagesAndSalaries = 16452.83
-
   const totalLaborExpenses = payrollTaxExpenses + wagesAndSalaries
 
   // Net Operating Expenses
   const netOperatingExpenses = totalOperatingExpenses - totalLaborExpenses
 
   // Cost of Goods Sold
-  // const totalCostOfGoodsSold = 66399.30
+  // totalCostOfGoodsSold
 
   // Overhead Multipliers
   const materialOverheadMultiplier = (totalLaborExpenses / totalCostOfGoodsSold) * (netOperatingExpenses / (totalCostOfGoodsSold + totalLaborExpenses)) + 1
   const laborOverheadMultiplier = (totalCostOfGoodsSold / totalLaborExpenses) * (netOperatingExpenses / (totalCostOfGoodsSold + totalLaborExpenses)) + 1
 
   // Direct Labor
-  // const employeeHourlyRate = 36
-  // const hoursWorkedOrEstimatedForJob = parseFloat(document.getElementById('hoursWorkedOrEstimatedForJob').value)
-
   totalDirectLaborCosts = employeeHourlyRate * hoursWorkedOrEstimatedForJob
 
   // Job Expenses
-  // const partsAndMaterials1 = parseFloat(document.getElementById('partsAndMaterials1').value|0)
-  // const partsAndMaterials2 = parseFloat(document.getElementById('partsAndMaterials2').value|0)
-  // const partsAndMaterials3 = parseFloat(document.getElementById('partsAndMaterials3').value|0)
-
   const totalJobExpenses = Number(partsAndMaterials1) + Number(partsAndMaterials2) + Number(partsAndMaterials3)
 
   // Job Costs
@@ -55,14 +45,11 @@ const calculatePrice = (user, parameters) => {
   const totalJobPrice = totalPartsPrice + totalLaborPrice
 
   // Profit
-  // const desiredPreTaxNetProfitMargin = 0.2
+  // desiredPreTaxNetProfitMargin
   const profitMultiplier = -1 / (desiredPreTaxNetProfitMargin - 1)
 
   // Sale Price to Customer
   const salePriceToCustomer = Math.round(totalJobPrice * profitMultiplier * 100) / 100
-
-  // document.getElementById('salePriceToCustomer').value = Math.round(salePriceToCustomer * 100) / 100
-
 
   return Promise.resolve(salePriceToCustomer)
 }
