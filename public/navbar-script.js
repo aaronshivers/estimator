1d9717fdf3cf1b8028f40e67338c72f16dad24b5
Aaron Shivers <aaron@moonmail.org>
2019-01-18 20:00:30 -0600

    Added info to navbar

--- public/navbar-script.js
+++ public/navbar-script.js
@@ -4,5 +4,6 @@ document.cookie ? null : navLinks.innerHTML += `<li class="nav-item"><a class="n
 document.cookie ? null : navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/signup">Signup</a></li>`
 document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/calculator">Calculator</a></li>` : null
 document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/settings">Settings</a></li>` : null
+document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/info">Info</a></li>` : null
 document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/profile">Profile</a></li>` : null
 document.cookie ? navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/logout">Logout</a></li>` : null
